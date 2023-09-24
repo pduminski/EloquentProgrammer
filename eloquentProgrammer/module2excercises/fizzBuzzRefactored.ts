df8a1073dd7ba17353c2c68a624dd41d9c8feae6
Pawel Duminski <pduminski@gmail.com>
2023-09-24 21:18:03 +0200

    remove file

--- eloquentProgrammer/module2excercises/fizzBuzzRefactored.ts
+++
@@ -1,9 +0,0 @@
-console.log("---> FizzBuzz <---");
-
-/* Napisz program ktory przy pomocy console.log wypisze:
-- Wszystkie liczby od 1 do 100 z 2 wyjatkami
-- zamiast liczb podzielnych przez 3 wydrukuj slowo "Fizz"
-- zamiast liczb podzielnych przez 5 wydrukuj slowo "Buzz"
-- zamiast liczb podzielnych zarowno przez 3 i 5 FizzBuzz*
-
-Program nadal ma drukowac podzielne tylko przez 3 i 5 jako Fizz i Buzz*/
